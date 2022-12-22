ad30a138fe2173e74b5895b8e24edc82b09a0e8d
Denis Stoyanouski <vandenst@tut.by>
2022-12-22 15:34:06 +0300

    created startMenu method

--- src/banking/Main.java
+++ src/banking/Main.java
@@ -1,7 +1,54 @@
 package banking;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        startMenu();
     }
+
+    private static void startMenu() {
+        String menu = """
+                1. Create an account
+                2. Log into account
+                0. Exit
+                """;
+        System.out.println(menu);
+        String input = input();
+        do {
+            switch (input) {
+                case "1":
+                    createAnAccount();
+                    break;
+                case "2":
+                    logIntoAccount();
+                    break;
+                case "0":
+                    exit();
+                    break;
+                default:
+                    System.out.println("Unknown command");
+                    break;
+            }
+        }
+            while (!"0".equals(input)) ;
+    }
+
+    private static void createAnAccount() {
+        System.out.println("create account");
+    }
+
+    private static void logIntoAccount() {
+        System.out.println("log in account");
+    }
+
+    private static void exit() {
+        System.exit(0);
+    }
+
+    private static String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
 }
